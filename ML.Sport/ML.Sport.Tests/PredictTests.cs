4bb890b1fb569b54f9185deafaae3c4b7b50c396
chris <chrisdunne88@outlook.com>
2019-01-12 17:03:37 +0000

    initial set up of the solution

---
+++ ML.Sport/ML.Sport.Tests/PredictTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.Sport.Tests
+{
+    [TestFixture]
+    public class PredictTests
+    {
+        [Test]
+        public void Predict_WhenInit_ReturnNew()
+        {
+            var output = new Predict();
+            Assert.IsInstanceOf(typeof(Predict), output);
+        }
+    }
+}
